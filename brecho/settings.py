09b57ca9f354a5e9701fccd6649c44e49d446341
C2002G <claudiogabrieldev@gmail.com>
2025-12-18 21:27:17 -0300

    finalizado

--- brecho/settings.py
+++ brecho/settings.py
@@ -32,7 +32,7 @@ if not SECRET_KEY:
     raise ValueError("A variável SECRET_KEY deve ser definida no arquivo .env")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 # Hosts permitidos
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
